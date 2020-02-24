47e21995d253798ec3df5a155ccfcf2ec3f50615
Ed van Veen <jevanveen@alum.mit.edu>
2020-02-24 12:44:39 -0800

    initial version

---
+++ R/telemR.R
@@ -0,0 +1,423 @@
+#' Read .asc files produced by starr telemetry and put them in tidy tibbles
+#'
+#' @param raw_asc .asc file produced by starr telemetry system. Use "new format" and "date-time in left column only" when exporting .asc
+#' @param meta_data data frame or tibble containing sample group information
+#' @param n_animals number of animals
+#' @param n_measurements number of measurments taken
+#' @param trim_na logical: should function remove time points containing NA values?
+#' @param trim_bad_probe logical: should function remove bad probes?
+#' @return a tidy telemetry tibble (tidy_telem)
+#' @export
+read_starr <- function(raw_asc, meta_data, n_animals = 8, n_measurements = 2, trim_na = T, trim_bad_probe = F){
+  tryCatch({
+    temp <- read_tsv(file = raw_asc, skip = (6 + (8 * n_measurements * n_animals)), col_types = "cnnnnnnnnnnnnnnnn")
+    if(trim_na == T){temp <- na.omit(temp)}
+    temp[[1]] <- as.POSIXct(temp[[1]])
+    colnames(temp)[1] <- "Time"
+    suppressWarnings(
+      samples <- read_tsv(file = raw_asc) %>%
+        t() %>%
+        grep(pattern = "Animal ID:", value = T) %>%
+        sub(pattern = "Animal ID: ", replacement = "") %>%
+        unique()
+    )
+    tlist <- list()
+    for(i in samples){
+      tlist[[i]] <- dplyr::select(temp, contains(i), contains("Time"))
+      if(grepl("Cnts", colnames(tlist[[i]][,1]))){colnames(tlist[[i]])[1] <- "Counts"}
+      if(grepl("Cnts", colnames(tlist[[i]][,2]))){colnames(tlist[[i]])[2] <- "Counts"}
+      if(grepl("Deg. C", colnames(tlist[[i]][,1]))){colnames(tlist[[i]])[1] <- "DegC"}
+      if(grepl("Deg. C", colnames(tlist[[i]][,2]))){colnames(tlist[[i]])[2] <- "DegC"}
+      tlist[[i]][,"Mouse"] <- i
+      tlist[[i]][,"Group"] <- colnames(meta_data[grep(i,meta_data)])
+    }
+    collapse <- bind_rows(tlist)
+    collapse$Mouse <- as.factor(collapse$Mouse)
+    collapse$Group <- as.factor(collapse$Group)
+    collapse$Time <- collapse$Time %>%
+      + lubridate::years(2000)
+    if(trim_bad_probe == T){collpase <- collpase %>% trim_bad_probe()}
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  return(collapse)
+
+}
+#' Read a folder of xlsx files produced by star-oddi telemetry and put them together in a tidy tibble
+#'
+#' @param folder a folder containing xlsx files produced by star-oddi emitters
+#' @param meta_data data frame or tibble containing sample group information
+#' @param trim_bad_probe logical: should function remove bad probes? Particularly useful for probes attached to mouse, rather than implanted
+#' @return a tidy telemetry tibble (tidy_telem)
+#' @export
+read_oddi <- function(folder, meta_data, trim_bad_probe = T){
+  tryCatch({
+    files <- list.files(folder)
+    temp <- list()
+    for(i in files){
+      temp[[i]] <- readxl::read_excel(paste0(folder, i))
+      temp[[i]]$Mouse <- i
+      colnames(temp[[i]])[2] <- "DegC"
+      temp[[i]]$Group <- colnames(meta_data[grep(sub(i, pattern = ".xlsx", replacement = ""),meta_data)])
+    }
+    ret <- bind_rows(temp)
+    ret$Mouse <- sub(ret$Mouse, pattern = ".xlsx", replacement = "") %>% factor()
+    ret <- mutate(ret, Time = as.POSIXct("1900-01-01 00:00:00") + ((`Date & Time`)*(86400)))
+    ret$Time <- lubridate::round_date(ret$Time, unit = "1 minute")
+    ret <- dplyr::select(ret, DegC, Time, Mouse, Group)
+    if(trim_bad_probe == T){
+      ret <- trim_bad_probe(ret)
+    }
+    return(ret)
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+#' Read tidy telemetry tibbles and export them as a multi page excel file.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param filename filename and path for saving excel file
+#' @param return_list logical: if true, returns multi page object as a list
+#' @return list or null
+#' @export
+export_telem <- function(tidy_telem, filename, return_list = F){
+  tryCatch({
+    tidy_telem$Time <- as.character(tidy_telem$Time)
+    tlist <- list()
+    for(i in levels(tidy_telem$Group)){
+      tlist[[paste0("Deg.C, ", i)]] <- tidy_telem %>%
+        filter(Group == i) %>%
+        select(-Counts) %>%
+        pivot_wider(names_from = Mouse, values_from = DegC)
+      tlist[[paste0("Counts, ", i)]] <- tidy_telem %>%
+        filter(Group == i) %>%
+        select(-DegC) %>%
+        pivot_wider(names_from = Mouse, values_from = Counts)
+    }
+    writexl::write_xlsx(tlist, path = filename)
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  if(return_list ==T){return(tlist)}
+}
+#' Graph tidy telemetry data.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param telem_var telemetry measurement to plot.
+#' @param one_day_avg collapse data into a 1 day average?
+#' @param group_by variable to group by. Either "Group" or "Mouse"
+#' @param tx_time vector of times (in posixct format) where to draw vertical dashed lines
+#' @param se_ribbon logical: display standard error ribbon?
+#' @return plot
+#' @export
+graph_telem <- function(tidy_telem, telem_var = "DegC", one_day_avg = F, group_by = "Mouse", tx_time = NULL, se_ribbon = T){
+  tryCatch({
+    if(one_day_avg == F){
+      tidy_telem$DST <- lubridate::dst(tidy_telem$Time)
+      split1 <- filter(tidy_telem, DST == T)
+      split2 <- filter(tidy_telem, DST == F)
+      rect_right1 <- split1$Time
+      lubridate::hour(rect_right1) <- 0
+      lubridate::minute(rect_right1) <- 0
+      rect_right1 <- rect_right1 %>% unique() + lubridate::hours(6)
+      rect_right2 <- split2$Time
+      lubridate::hour(rect_right2) <- 0
+      lubridate::minute(rect_right2) <- 0
+      rect_right2 <- rect_right2 %>% unique() + lubridate::hours(6)
+      rect_right <- c(rect_right1, rect_right2)
+      rect_right <- rect_right[rect_right %within% interval(first_time(tidy_telem$Time), last_time(tidy_telem$Time))]
+      rectangles <- data.frame(
+        xmin = rect_right - lubridate::hours(12),
+        xmax = rect_right,
+        ymin = -Inf,
+        ymax = Inf)
+      time1 <- c(first_time(grep("12:00:00", x = tidy_telem[["Time"]], value = T)), first_time(grep("00:00:00", x = tidy_telem[["Time"]], value = T))) %>%
+        as.POSIXct() %>%
+        first_time()
+      time2 <- c(last_time(grep("12:00:00", x = tidy_telem[["Time"]], value = T)), last_time(grep("00:00:00", x = tidy_telem[["Time"]], value = T))) %>%
+        as.POSIXct() %>%
+        last_time()
+      p1 <- ggplot(data = tidy_telem, aes(x = tidy_telem[["Time"]], y = tidy_telem[[telem_var]], color = tidy_telem[[group_by]], group = tidy_telem[[group_by]])) +
+        theme_classic() +
+        geom_rect(inherit.aes = F, data = rectangles, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+                  fill='gray80', alpha=0.8) +
+        stat_summary(geom="line", fun.y=mean) +
+        xlab("Time") +
+        ylab(telem_var) +
+        scale_color_discrete(name = group_by) +
+        scale_x_datetime(breaks = seq(time1, time2, by = 43200)) +
+        theme(axis.text.x = element_text(angle = -90))
+
+    } else {
+      tdiff <- last_time(tidy_telem[["Time"]]) - first_time(tidy_telem[["Time"]])
+      tdiff <- tdiff %>%
+        round(2) %>%
+        as.numeric()
+      tidy_telem <- collapse_telem(tidy_telem, telem_var = telem_var)
+      last.break <- tidy_telem$Time %>% last() %>% as.character()
+      p1 <- ggplot(data = tidy_telem, aes(x = tidy_telem[["Time"]], y = tidy_telem[[telem_var]], color = tidy_telem[[group_by]], group = tidy_telem[[group_by]])) +
+        theme_classic() +
+        geom_rect(aes(xmin = "12:00:00",
+                      xmax = last.break,
+                      ymin = -Inf, ymax = Inf), color = "white", fill = "lightgrey") +
+        stat_summary(geom = "line", fun.y = mean) +
+        xlab("Zeitgeber Time") +
+        ylab(telem_var) +
+        scale_color_discrete(name = group_by) +
+        scale_x_discrete(breaks = c("00:00:00", "06:00:00", "12:00:00", "18:00:00", last.break)) +
+        theme(axis.text.x = element_text(angle = -90)) +
+        ggtitle(paste0(tdiff, " day mean"))
+    }
+    if(!is.null(tx_time)){
+      p1 <- p1 +
+        geom_vline(xintercept = as.POSIXct(tx_time), linetype = "dashed")
+    }
+    if(se_ribbon == T){
+      p1 <- p1 +
+        stat_summary(geom="ribbon", fun.data = mean_se, aes(fill = tidy_telem[[group_by]]), alpha = .5, color = NA, show.legend = F)
+    }
+
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  return(p1)
+}
+#' Trim tidy telemetry data.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param start_time start time for new output file
+#' @param end_time end time for new output file
+#' @param keep_groups optionally keep selected groups as defined in meta data. Default (NULL) is to keep all.
+#' @param keep_mice optionally keep defined mice. Default (NULL) is to keep all.
+#' @return a tidy telemetry tibble
+#' @export
+trim_telem <- function(tidy_telem, start_time, end_time, keep_groups = NULL, keep_mice = NULL){
+  tryCatch({
+    pt <- tidy_telem %>%
+      filter(Time >= start_time & Time <= end_time)
+    if(!is.null(keep_groups)){
+      pt <- pt %>%
+        filter(Group %in% keep_groups)
+    }
+    if(!is.null(keep_mice)){
+      pt <- pt %>%
+        filter(Mouse %in% keep_mice)
+    }
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  return(pt)
+}
+#' linear mixed effects model of telemetry data.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param telem_var telemetry measurement to analyze
+#' @param collapse_first logical. collapse to a 1 day average before analysis? recommended.
+#' @return list containing anova and models built
+#' @export
+lme_telem <- function(tidy_telem, telem_var = "DegC", collapse_first = T){
+  tryCatch({
+    if(collapse_first == T){
+      tidy_telem <- tidy_telem %>% collapse_telem()
+    }
+    basemjev <<- formula(paste0(telem_var," ~ 1"))
+    groupmjev <<- formula(paste0(telem_var," ~ Group"))
+    timemjev <<- formula(paste0(telem_var," ~ Group + Time"))
+    intermjev <<- formula(paste0(telem_var," ~ Group * Time"))
+    baseline <- nlme::lme(basemjev, random = ~1 | Mouse/Group, data = tidy_telem, method = "ML")
+    Group <- nlme::lme(groupmjev, random = ~1 | Mouse/Group, data = tidy_telem, method = "ML")
+    Time <- nlme::lme(timemjev, random = ~1 | Mouse/Group, data = tidy_telem, method = "ML")
+    GroupXTime <- nlme::lme(intermjev, random = ~1 | Mouse/Group, data = tidy_telem, method = "ML")
+    statlist <- list()
+    statlist[["AOV"]] <- anova(baseline, Group, Time, GroupXTime)
+    statlist[["GroupSummary"]] <- summary(Group)
+    statlist[["TimeSummary"]] <- summary(Time)
+    statlist[["InteractionSummary"]] <- summary(GroupXTime)
+    rm(basemjev, groupmjev, timemjev, intermjev, pos = ".GlobalEnv")
+    return(statlist)
+
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+#' multiple comparisons of telemetry data.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param formula
+#' @param collapse_first optionally keep defined mice. Default (NULL) is to keep all
+#' @return list containing anova and models built
+#' @export
+multcomp_telem <- function(tidy_telem, formula, plot_or_table = "plot", collapse_first = T){
+  tryCatch({
+    if(collapse_first == T){
+      tidy_telem <- tidy_telem %>% collapse_telem()
+    }
+    multcomp <- tidy_telem %>%
+      group_by(Time) %>%
+      do(broom::tidy(t.test(formula, data = .))) %>%
+      ungroup %>%
+      mutate(padj = p.adjust(p.value, method = "fdr"))
+    if(plot_or_table == "plot"){
+      p1 <- ggplot(data = multcomp, aes(x = Time, y = -log10(padj))) +
+        geom_point() +
+        theme_classic() +
+        geom_hline(yintercept = 1.30103, linetype = "dashed") +
+        geom_hline(yintercept = 2, linetype = "dashed") +
+        geom_hline(yintercept = 3, linetype = "dashed") +
+        geom_hline(yintercept = 4, linetype = "dashed") +
+        ylim(0,5)
+
+      if(collapse_first == T){
+        p1 <- p1 + scale_x_discrete(breaks = c("06:00:00", "12:00:00", "18:00:00", "00:00:00", "05:55:00"))
+      } else {
+        first_time <- function(time_vector){range(time_vector)[1]}
+        last_time <- function(time_vector){range(time_vector)[2]}
+        time1 <- c(first_time(grep("12:00:00", x = tidy_telem[["Time"]], value = T)), first_time(grep("00:00:00", x = tidy_telem[["Time"]], value = T))) %>%
+          as.POSIXct() %>%
+          first_time()
+        time2 <- c(last_time(grep("12:00:00", x = tidy_telem[["Time"]], value = T)), last_time(grep("00:00:00", x = tidy_telem[["Time"]], value = T))) %>%
+          as.POSIXct() %>%
+          last_time()
+        p1 <- p1 + scale_x_datetime(breaks = seq(time1, time2, by = 43200))
+      }
+
+    }
+    else {
+      p1 <- multcomp
+    }
+    return(p1)
+
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+#' average telemetry data over user defined windows. very useful when data appear very noisy/oversampled
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param output_window what should final output increment be?
+#' @param telem_var which measurement to fuzz. Default is "Both" which equals "DegC" and "Counts"
+#' @return a time fuzzed tidy telemetry tibble
+#' @export
+fuzz_telem <-function(tidy_telem, output_window, telem_var = "Both"){
+  tryCatch({
+    tidy_telem$Time <- lubridate::round_date(tidy_telem$Time, unit = output_window)
+    if(telem_var == "Both"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(Counts = mean(Counts, na.rm = T), DegC = mean(DegC, na.rm = T))
+    }
+    if(telem_var == "DegC"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(DegC = mean(DegC, na.rm = T))
+    }
+    if(telem_var == "Counts"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(Counts = mean(Counts, na.rm = T))
+    }
+    return(tidy_telem)
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+#' automatically detect and drop bad probes once they stop working. very useful for probes attached to mouse tails as they can fall off.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param DegC_threshold what temperature in C is considered a spuriously low reading?
+#' @param over_how_long temp values should be below the threshold over how big of a time window before probe is considered bad?
+#' @return a tidy telemetry tibble with bad probes removed starting a the point that they meet "bad" criteria
+#' @export
+trim_bad_probe <- function(tidy_telem, DegC_threshold = 23, over_how_long = "4 hours"){
+  tryCatch({
+    fuzzed <- fuzz_telem(tidy_telem, output_window = over_how_long, telem_var = "DegC")
+    for(i in levels(tidy_telem$Mouse)){
+      this.ms.fuzzed <- fuzzed %>%
+        filter(Mouse == i)
+      if(min(this.ms.fuzzed$DegC) < DegC_threshold) {
+        first.time <- first(this.ms.fuzzed$Time)
+        last.time.ms <- this.ms.fuzzed %>%
+          filter(DegC < DegC_threshold)
+        last.time <- last.time.ms$Time %>%
+          first() %>%
+          - lubridate::hours(sub(" hours", "", over_how_long) %>% as.numeric())
+        this.ms.fixed <- tidy_telem %>%
+          filter(Mouse == i) %>%
+          trim_telem(start_time = first.time, end_time = last.time)
+        tidy_telem <- tidy_telem %>%
+          filter(Mouse != i) %>%
+          bind_rows(this.ms.fixed)
+      }}
+    return(tidy_telem)
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+#' collapse all days in a tidy telem tibble, averaging read values
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param telem_var which measurements should be collapsed?
+#' @return a tidy telemetry tibble of length 1d. Note time is now of class factor
+#' @export
+collapse_telem <- function(tidy_telem, telem_var = "Both"){
+  tryCatch({
+    tidy_telem <- zeitgeber_time(tidy_telem)
+    t2 <- as.POSIXct("2020-01-11 23:55:00 PST")
+    t3 <- as.POSIXct("2020-01-11 00:00:00 PST")
+    levs <- c(seq(t3, t2, by=300)) %>%
+      format("%H:%M:%S")
+    tidy_telem$Time <- tidy_telem$Time %>%
+      format("%H:%M:%S") %>%
+      factor(levels = levs)
+    if(telem_var == "Both"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(Counts = mean(Counts, na.rm = T), DegC = mean(DegC, na.rm = T))
+    }
+    if(telem_var == "DegC"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(DegC = mean(DegC, na.rm = T))
+    }
+    if(telem_var == "Counts"){
+      tidy_telem <- tidy_telem %>%
+        group_by(Time, Mouse, Group) %>%
+        summarise(Counts = mean(Counts, na.rm = T))
+    }
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  return(tidy_telem)
+}
+#' convert time to zeitgeber (lightgiver) time: 0 = lights on, 12 = lights off.
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param undo logical. if true, converts ZT to clock time.
+#' @param lights_on_DST at what time do the lights in the room come on, during DST. This algorithm assumes that the room light schedule does not adjust for DST
+#' @return a tidy telemetry tibble converted to zeitgeber time.
+#' @export
+zeitgeber_time <- function(tidy_telem, undo = F, lights_on_DST = 6){
+  tryCatch({
+    if(undo == F){
+      tidy_telem$DST <- lubridate::dst(tidy_telem$Time)
+      split1 <- filter(tidy_telem, DST == T)
+      split2 <- filter(tidy_telem, DST == F)
+      split1$Time <- split1$Time %>% - lubridate::hours(lights_on_DST)
+      split2$Time <- split2$Time %>% - lubridate::hours(lights_on_DST + 1)
+      ret <- bind_rows(split1, split2) %>%
+        select(-DST)
+    } else {
+      tidy_telem$DST <- lubridate::dst(tidy_telem$Time)
+      split1 <- filter(tidy_telem, DST == T)
+      split2 <- filter(tidy_telem, DST == F)
+      split1$Time <- split1$Time %>% + lubridate::hours(lights_on_DST)
+      split2$Time <- split2$Time %>% + lubridate::hours(lights_on_DST + 1)
+      ret <- bind_rows(split1, split2) %>%
+        select(-DST)
+    }
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  return(ret)
+}
+#' add a logical column to tidy telem tibble indicating if lights are on (true)
+#'
+#' @param tidy_telem a tidy telemetry tibble, as produced by read_starr or read_oddi
+#' @param lights_on_DST at what time do the lights in the room come on, during DST. This algorithm assumes that the room light schedule does not adjust for DST
+#' @return a tidy telemetry tibble with logical column "DST" added. Time is still clock time.
+light_dark <- function(tidy_telem, lights_on_DST = 6){
+  tidy_telem <- tidy_telem %>%
+    zeitgeber_time(lights_on_DST = lights_on_DST)
+  tidy_telem$Light <- lubridate::am(tidy_telem$Time)
+  tidy_telem <- tidy_telem %>%
+    zeitgeber_time(undo = T, lights_on_DST = lights_on_DST)
+  return(tidy_telem)
+}
+#' return the earliest time in a time vector
+#'
+#' @param time_vector a vector of times in class posixct or anything that works with range()
+#' @return the earliest time in the vector
+first_time <- function(time_vector){range(time_vector)[1]}
+#' return the latest time in a time vector
+#'
+#' @param time_vector a vector of times in class posixct or anything that works with range()
+#' @return the latest time in the vector
+last_time <- function(time_vector){range(time_vector)[2]}
